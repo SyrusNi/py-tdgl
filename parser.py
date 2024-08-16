16c0f67ea39b9e4e9bd1b79f78fd13a1c5e4be02
咕噜布 <nishengding@126.com>
2024-08-16 18:40:06 -0400

    commit

--- parser.py
+++ parser.py
@@ -12,14 +12,14 @@ def parse_args():
 
     #layer parameters
     parser.add_argument('--length_units', type=str, default='um')
-    parser.add_argument('--coherence_length', type=float, default=0.5)
+    parser.add_argument('--coherence_length', type=float, default=1/3)
     parser.add_argument('--london_lambda', type=float, default=2)
     parser.add_argument('--thickness', type=float, default=0.1)
     parser.add_argument('--gamma', type=float, default=1, help='inelastic electron-phonon scattering, gapless superconductors gamma = 0 or dirty gapped superconductors gamma > 0')
 
     #device geometry
-    parser.add_argument('--width', type=float, default=5)
-    parser.add_argument('--length', type=float, default=5)
+    parser.add_argument('--width', type=float, default=6)
+    parser.add_argument('--length', type=float, default=6)
 
     #make mesh
     parser.add_argument('--max_edge_length', type=float, default=0.25, help='should be smaller than coherence length')
@@ -33,12 +33,12 @@ def parse_args():
     parser.add_argument('--save_every', type=float, default=100)
     
     #Externel Fields
-    parser.add_argument('--setpoints', type=ndarray, default=np.array([[0, 50, 300], [0, 0.5, 0.5]]).T)
+    parser.add_argument('--setpoints', type=ndarray, default=np.array([[0, 300], [0.39, 0.39]]).T)
     parser.add_argument('--value', type=float, default=1.0)
 
     #save options
     parser.add_argument('--output_directory', type=str, default='solutions/0816')
-    parser.add_argument('--output_file', type=str, default='no_temp_change')
+    parser.add_argument('--output_file', type=str, default='showup_300')
     #parser.add_argument('--plot_time_factor', type=bool, default=False)
     parser.add_argument('--make_animation', type=bool, default=False)
 
